013af7c6fb7a7363a9997ed333dacea136730e5d
Shubham kumar <shubhamkr5051@gmail.com>
2021-08-11 01:05:15 +0530

    fix add new card data into local storage successfully

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 const taskContainer = document.querySelector(".task__container");
 const globalStore = [];
+
+const generateNewCard = (taskData) =>
+  `<div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id} >
+ <div class="card">
+     <div class="card-header d-flex justify-content-end gap-2">
+         <button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i></button>
+         <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+     </div>
+     <div class="card-body">
+         <img src=${taskData.imageUrl}
+             class="card-img-top" alt="task image">
+         <h5 class="card-title mt-3 text-black fw-bold">${taskData.taskTitle}</h5>
+         <p class="card-text">${taskData.taskDescription}</p>
+         <a href="#" class="btn btn-info fw-bold">${taskData.taskType}</a>
+     </div>
+ </div>
+</div>`;
+
+const loadIntialCardData = () => {
+  //local storage to get tasky card data
+  const getCardData = localStorage.getItem("maal");
+
+  //convert to normal object [converting array of object into object to object]
+  const { cards } = JSON.parse(getCardData);
+
+  //loop over those array of task object to create HTML Card , inject to DOM
+  cards.map((cardObject) => {
+    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+    //Upadte our globalStore
+    globalStore.push(cardObject);
+  });
+};
+
 //Arrow function which will get trigerred by save changes button inside modal
 const saveChanges = () => {
   const taskData = {
@@ -10,25 +43,8 @@ const saveChanges = () => {
     taskType: document.getElementById("tasktype").value,
     taskDescription: document.getElementById("taskdescription").value,
   };
-  //  creating new card by getting user data from modal
-  const newCard = `
-  <div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id} >
-            <div class="card">
-                <div class="card-header d-flex justify-content-end gap-2">
-                    <button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i></button>
-                    <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
-                </div>
-                <div class="card-body">
-                    <img src=${taskData.imageUrl}
-                        class="card-img-top" alt="task image">
-                    <h5 class="card-title mt-3 text-black fw-bold">${taskData.taskTitle}</h5>
-                    <p class="card-text">${taskData.taskDescription}</p>
-                    <a href="#" class="btn btn-info fw-bold">${taskData.taskType}</a>
-                </div>
-            </div>
-        </div>`;
 
-  taskContainer.insertAdjacentHTML("beforeend", newCard);
+  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
 
   //   creating local database
 
