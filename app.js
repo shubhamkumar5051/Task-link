57836efd17fe1a1d5d46f6ac8d261078ef017e01
Shubham kumar <shubhamkr5051@gmail.com>
2021-08-11 13:59:47 +0530

    feat delete card function

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const taskContainer = document.querySelector(".task__container");
-const globalStore = [];
+let globalStore = [];
+
+//generate new card
 
 const generateNewCard = (taskData) =>
-  `<div class="col-sm-12 col-md-6 col-lg-4" id=${taskData.id} >
+  `<div class="col-sm-12 col-md-6 col-lg-4" >
  <div class="card">
      <div class="card-header d-flex justify-content-end gap-2">
          <button type="button" class="btn btn-success"><i class="fas fa-pencil-alt"></i></button>
-         <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+         <button type="button" class="btn btn-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)" > <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
      </div>
      <div class="card-body">
          <img src=${taskData.imageUrl}
@@ -18,6 +20,31 @@ const generateNewCard = (taskData) =>
  </div>
 </div>`;
 
+//delete function
+
+const deleteCard = (event) => {
+  event = window.event;
+  //fetching the id of event
+  const targetID = event.target.id;
+  const tagName = event.target.tagName;
+
+  globalStore = globalStore.filter((cardObject) => {
+    cardObject.id !== targetID;
+    localStorage.setItem("maal", JSON.stringify({ cards: globalStore }));
+  });
+
+  if (tagName === "BUTTON") {
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode
+    );
+  } else {
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode.parentNode
+    );
+  }
+};
+
+//this function will get trigerred by "onLoad" attribute inside body tag
 const loadIntialCardData = () => {
   //local storage to get tasky card data
   const getCardData = localStorage.getItem("maal");
@@ -52,5 +79,5 @@ const saveChanges = () => {
 
   localStorage.setItem("maal", JSON.stringify({ cards: globalStore })); // maal is unique id for local storage
 
-  //converting (object of object) into (Array of object) with help of stringify
+  //converting (object of object) into (Array of object) with help of stringify. and stringfy store the data in key value format
 };
